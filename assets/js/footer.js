72e463652658660680b9925d41505305faaf57f9
DhanushS212003 <dhanushs@boxs.in>
2024-08-31 22:14:22 +0530

    make the search section and banner section responsive with sliding banner effect

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,21 +1,21 @@
 const footer = `<footer>
         <div class = "footer_container">
-            <div class = "left_foot">
+            <div class = "left_section">
                 <img class = "footer_logo" src = "/assets/images/cattles/logo2.png" alt = "logo">
                 <p class = "footer_content"> Kowmart.com is an online marketplace established with the aim to strengthen Livestock
                     Economy of the country. It helps livestock enterprises/self-employed/individuals to achieve their
                     full potential while ensuring livestock wellbeing.</p>
             </div>
 
-            <div class = "right_foot">
-                <h4 class = "text-body-tertiary"> CONNECT WITH US</h4>
-                <p class = "mt-2"> Email: info@kowmart.com</p>
-                <p class = "mt-2"> Ph.no: 1234567890</p>
-                <p class = "mt-2"> Address:  No.3/5, Anna Salai, West Mambalam, Chennai - 33. </p>
+            <div class = "right_section">
+                <h5 class = "text-body-tertiary"> CONNECT WITH US</h5>
+                <p class = "footer_com_info"> Email: <span class = "fw-bold"> info@kowmart.com </span></p>
+                <p class = "footer_com_info"> Ph.no:  <span class = "fw-bold"> 1234567890 </span></p>
+                <p class = "footer_com_info lh-base"> Address:  <span class = "fw-bold"> Chennai, India </span></p>
                 <div class = "com_icons">
-                    <i class = "fa-brands fa-facebook"></i>
-                    <i class = "fa-brands fa-whatsapp"></i>
-                    <i class = "fa-brands fa-instagram"></i>
+                    <i class = "fa-brands fa-facebook fs-5"></i>
+                    <i class = "fa-brands fa-whatsapp fs-5"></i>
+                    <i class = "fa-brands fa-instagram fs-5"></i>
                 </div>
             </div>
         </div>
