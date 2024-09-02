f32489f213d130984024ea871159b3bb1878e2d4
DhanushS212003 <dhanushs@boxs.in>
2024-09-02 17:22:07 +0530

    moved banner functionality script to common file

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,43 @@ function closeNav() {
   document.querySelector("i.close_icon").style.display = "none";
   document.querySelector("i.bars_icon").style.display = "block";
 }
+
+let bannerIndex = "0";
+let bannerLastIndex = "";
+
+function moveBanners(banners) {
+  bannerLastIndex = banners.length - 1 + "";
+
+  setBanner("0");
+
+  document.querySelectorAll(".banner_dots").forEach((each) => {
+    each.addEventListener("click", (event) => {
+      setBanner(each.id);
+    });
+  });
+}
+
+function setBanner(index) {
+  bannerIndex = index + "";
+
+  document.querySelectorAll(".banner_slide").forEach((slide, i) => {
+    slide.style.transform = `translateX(-${index * 100}%)`;
+    slide.style.backgroundImage = `url(${banners[i]})`;
+  });
+
+  document.querySelectorAll(".banner_dots").forEach((dot, i) => {
+    dot.classList.toggle("active_dot", i == index);
+  });
+}
+
+function movePreviousBanner() {
+  bannerIndex == 0
+    ? setBanner(bannerLastIndex)
+    : setBanner(parseInt(bannerIndex) - 1);
+}
+
+function moveNextBanner() {
+  bannerIndex == bannerLastIndex
+    ? setBanner("0")
+    : setBanner(parseInt(bannerIndex) + 1);
+}
